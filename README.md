2325168c274f311d4b11d57daeffe71f229b7efc
Thomas Nilefalk <thomas@nilefalk.se>
2023-11-24 20:41:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cgreen Test Provider
+
+This Visual Studio Code extension provides Cgreen test integration in the VSCode Test View.
+
+Install the extension and open a C or C++ file. Open the Test View to discover tests written using Cgreen.
+
+## Cgreen Test Discovery
+
+Any dynamically linked library will be inspected for Cgreen tests.
+
+## Status
+
+Infancy ;-)
---
+++ src/extension.ts
@@ -0,0 +1,44 @@
+import * as vscode from 'vscode';
+
+export async function activate(context: vscode.ExtensionContext) {
+    const controller = vscode.tests.createTestController('cgreenController', 'Cgreen Tests');
+    context.subscriptions.push(controller);
+
+    controller.createRunProfile('Run Tests', vscode.TestRunProfileKind.Run, runHandler, true);
+
+    controller.resolveHandler = async item => {
+        if (!item) {
+            await discoverAllTests();
+            return;
+        }
+        await resolveTestItem(item);
+    };
+
+    async function runHandler(request: vscode.TestRunRequest, cancellation: vscode.CancellationToken) {
+        const run = controller.createTestRun(request);
+        for (const test of request.include ?? gatherTestItems(controller.items)) {
+            if (cancellation.isCancellationRequested) {
+                run.skipped(test);
+                continue;
+            }
+            run.passed(test, 0); // or run.failed(test, 0) based on test result
+        }
+        run.end();
+    }
+
+    async function discoverAllTests() {
+		// Have to discover at least one test for the test view to show up
+        const testItem = controller.createTestItem("testId", "testName");
+		controller.items.add(testItem);
+    }
+
+    async function resolveTestItem(item: vscode.TestItem) {
+        // Logic to resolve a single test item
+    }
+
+    function gatherTestItems(collection: vscode.TestItemCollection) {
+        const items: vscode.TestItem[] = [];
+        collection.forEach(item => items.push(item));
+        return items;
+    }
+}
