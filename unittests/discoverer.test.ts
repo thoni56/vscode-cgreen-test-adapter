9aef446525041cff17397eb6d81675178bea50fb
Thomas Nilefalk <thomas@nilefalk.se>
2023-12-03 22:32:48 +0100

    Add test for discovering cgreen .so file

--- unittests/discoverer.test.ts
+++ unittests/discoverer.test.ts
@@ -39,13 +39,13 @@ describe("Discoverer Module", () => {
     });
 
     it("should discover no files in a workspace with no .so files", async () => {
-        findFilesStub.resolves([vscode.Uri.file("a.exe")]);
+        findFilesStub.resolves([vscode.Uri.file("non-cgreen.exe")]);
         expect(await discoverCgreenFiles(mockedWorkspaceFolder)).toEqual([]);
     });
 
     it("should not discover any .so file as a Cgreen test file if it has no tests", async () => {
         // Mock findFiles to return .so files
-        findFilesStub.resolves([vscode.Uri.file("a.so")]);
+        findFilesStub.resolves([vscode.Uri.file("non_cgreen_file.so")]);
 
         // Mock exec to simulate cgreen-runner not finding tests
         execStub.yields(null, "No tests found");
@@ -56,5 +56,30 @@ describe("Discoverer Module", () => {
 
         // Assertions
         expect(discoveredFiles).toHaveLength(0);
-    });    
+    });
+    
+    it("should discover one .so file as a Cgreen test file if it has tests", async () => {
+        execStub.callsFake((command: string, ...args: any[]) => {
+            // Exec is overloaded so we can't use a single signature,
+            // collect all variants and pull out the callback...
+            const callback = args.find(arg => typeof arg === 'function'); 
+
+            if (command.includes('non_cgreen_file.so')) {
+              callback(null, 'No tests found', '');
+            } else if (command.includes('cgreen_tests.so')) {
+              callback(null, 'Running "options_tests" (38 tests)...\n  "Options": 94 passes, 2 skipped in 20ms.\nCompleted "options_tests": 94 passes, 2 skipped in 20ms.', ''); 
+            }
+        });
+        
+        // Mock findFiles to return .so files
+        findFilesStub.resolves([vscode.Uri.file("non_cgreen_file.so"), vscode.Uri.file("cgreen_tests.so")]);
+
+        const discoveredFiles = await discoverCgreenFiles(
+            mockedWorkspaceFolder
+        );
+
+        // Assertions
+        expect(discoveredFiles).toHaveLength(1);
+        expect(discoveredFiles[0].path).toEqual("cgreen_tests.so");
+    });  
 });
