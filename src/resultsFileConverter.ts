fa21dd9ef95f9605ca973e81414012f7b9b66f94
Thomas Nilefalk <thomas@nilefalk.se>
2023-12-12 18:04:15 +0100

    Add all children from the XML

--- src/resultsFileConverter.ts
+++ src/resultsFileConverter.ts
@@ -2,10 +2,18 @@ import * as vscode from 'vscode';
 import { createTestItem } from "./testItem";
 import { parseXmlFile } from './xmlParser';
 
-export function testItemsFromResultsFile(resultsFile: vscode.Uri) : vscode.TestItem {
+export function testItemsFromResultsFile(resultsFileUri: vscode.Uri) : vscode.TestItem {
     // Create a tree of ExtendedTestItems representing all results in the file
-    const xml = parseXmlFile(resultsFile);
-    const suite = createTestItem("id1", xml.testsuite.name, resultsFile);
-    suite.children.add(createTestItem("id2", "label2", resultsFile));
+    const xml = parseXmlFile(resultsFileUri);
+    const suite:vscode.TestItem = createTestItem(`testsuite%${xml.testsuite.name}`, xml.testsuite.name, resultsFileUri);
+
+    if (xml.testsuite.testcase) {
+        const testCaseArray = Array.isArray(xml.testsuite.testcase) ? xml.testsuite.testcase : [ xml.testsuite.testcase ];
+        testCaseArray.forEach((testcase:any) => {
+            const childId = `testcase%${xml.testsuite.name}%${testcase.name}`;
+            suite.children.add(createTestItem(childId, testcase.name, resultsFileUri));
+        });
+    }
+
     return suite;
 }
